e660d98c3c21ab84b5c0fbfe320498d74bd79ef8
orcax7 <126786165+orcax7@users.noreply.github.com>
2024-05-21 12:30:05 +0700

    update
    update add

--- src/Add/add.js
+++ src/Add/add.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
-import '../App.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 function Add() {
     const [userData, setUserData] = useState({
-        name: '',
-        userName: '',
-        email: '',
-        phoneNumber: ''
+        name: '', 
+        username: '', 
+        address: '', 
+        phoneNumber: '' 
     });
     const navigate = useNavigate();
 
@@ -16,7 +15,7 @@ function Add() {
         const { name, value } = e.target;
         setUserData({
             ...userData,
-            [name]: value   
+            [name]: value
         });
     };
 
@@ -24,13 +23,14 @@ function Add() {
         e.preventDefault();
         console.log(userData);
         try {
-            const response = await axios.post("http://localhost:3001/user", userData);
+            const response = await axios.post("http://localhost:55174/user/add", userData);
             console.log("User added:", response.data);
             setUserData({
-                name: '',
-                userName: '',
-                email: '',
-                phoneNumber: ''
+            name: '', 
+            username: '', 
+            address: '', 
+            phoneNumber: '' 
+
             });
             navigate('/');
         } catch (error) {
@@ -47,19 +47,19 @@ function Add() {
                     <input type="text" id="name" name="name" value={userData.name} onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="userName">Username:</label>
-                    <input type="text" id="userName" name="userName" value={userData.userName} onChange={handleChange} />
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" value={userData.username} onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" value={userData.email} onChange={handleChange} />
+                    <label htmlFor="address">Address:</label>
+                    <input type="text" id="address" name="address" value={userData.address} onChange={handleChange} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="phoneNumber">Phone Number:</label>
                     <input type="text" id="phoneNumber" name="phoneNumber" value={userData.phoneNumber} onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                    <button type="submit" className="btn btn-success">Save</button>
+                    <button type="submit" className="btn btn-success">ADD</button>
                 </div>
             </form>
         </div>
